aeb42e678c08efa8aed218f04c9f6529cdfb9c1f
Jirlan <jirlansouza08@gmaial.com>
2022-07-27 21:55:15 -0400

    Created the Header component test

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,7 @@ module.exports = {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
   },
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(scss|sass|css)": "identity-obj-proxy"
+  }
 };
--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { ActiveLink } from '.';
 
@@ -14,13 +14,13 @@ jest.mock('next/router', () => {
 
 describe('ActiveLink component', () => {
   it('active link renders correctly', () => {
-    const { getByText } = render(
+    render(
       <ActiveLink href="/" activeClassName="active">
         <a>Home</a>
       </ActiveLink>
     );
 
-    expect(getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Home')).toBeInTheDocument();
   });
 
   it('adds active class if the link as currently active', () => {
